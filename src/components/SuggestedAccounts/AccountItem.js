8f00819161d70ca632acd4bb922227102479e87a
tuananh191103 <letuananh19112003@gmail.com>
2024-10-10 17:26:36 +0700

    first commit

---
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -0,0 +1,47 @@
+import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountPreview from './AccountPreview/AccountPreview';
+import styles from './SuggestedAccounts.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    const renderPreview = (props) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview />
+                </PopperWrapper>
+            </div>
+        );
+    };
+
+    return (
+        <div>
+            <Tippy interactive delay={[800, 0]} offset={[-20, 0]} placement="bottom" render={renderPreview}>
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://yt3.googleusercontent.com/oN0p3-PD3HUzn2KbMm4fVhvRrKtJhodGlwocI184BBSpybcQIphSeh3Z0i7WBgTq7e12yKxb=s900-c-k-c0x00ffffff-no-rj"
+                        alt=""
+                    />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>letuananh</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Lê Tuấn Anh</p>
+                    </div>
+                </div>
+            </Tippy>
+        </div>
+    );
+}
+
+AccountItem.propTypes = {};
+
+export default AccountItem;
